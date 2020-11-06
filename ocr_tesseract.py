7d09c9855105f35dc902842292cc3cc73ea33974
Konstantin Roppel <k.roppel@epages.com>
2020-11-06 02:36:09 +0100

    .

--- ocr_tesseract.py
+++ ocr_tesseract.py
@@ -2,7 +2,6 @@ from pytesseract import image_to_string
 
 def recognize_digit_tesseract_ocr(image, i, j):
     digits = ["1","2","3","4","5","6","7","8","9"]
-
     result = image_to_string(image, config = r'--psm 10')[0]
     result = result if (result in digits) else "0"
 
