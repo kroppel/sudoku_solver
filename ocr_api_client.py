7ef9a3e7ecd709426d9e57ec85ed7ff35727a33d
Konstantin Roppel <k.roppel@epages.com>
2020-11-15 14:12:15 +0100

    Moved api_key read-in into ocr_api_client

--- ocr_api_client.py
+++ ocr_api_client.py
@@ -3,25 +3,32 @@ import io
 import requests
 import json
 
-def request_ocr(image, api_key):
+def request_ocr(image):
+    # Read Api-Key from text file (insert your own here!)
+    api_key = open("api_key.txt", "r").readline()
+
     # Encode and compress Image
     _, compressed_image = cv2.imencode(".jpg", image, [1, 100])
+
     # Get bytes of image
     image_bytes = io.BytesIO(compressed_image)
+
     # send POST request to OCR API and receive result
     response = requests.post(url="https://api.ocr.space/parse/image", files = {"field.jpg": image_bytes}, data = {"apikey": api_key, "ocrengine": 2, "scale": True})
+    
     # "unwrap" result and return the relevant information
     response_content = json.loads(response.content.decode())
+    
     #print(str(response_content))
     parsed_text = response_content.get("ParsedResults")[0].get("ParsedText")
 
     return parsed_text
 
-def recognize_digit_ocr_space(image, api_key, i, j):
+def recognize_digit_ocr_space(image, i, j):
     digits = ["1","2","3","4","5","6","7","8","9"]
-    ocr_result = request_ocr(image, api_key)
+    ocr_result = request_ocr(image)
     result = 0
-    #print("Result: " + str(ocr_result))
+    
     try:
         if ocr_result in digits:
             result = int(ocr_result)
